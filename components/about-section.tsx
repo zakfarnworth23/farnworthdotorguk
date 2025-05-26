256f42dcecba624fe36fd45d7c7f4c34962875dc
Zak Farnworth <130570087+zakfarnworth23@users.noreply.github.com>
2025-05-26 14:35:31 +0100

    Update about-section.tsx

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -19,22 +19,18 @@ export default function AboutSection() {
             From Apprentice to Advocate: Zak Farnworth’s journey starting at 16.
           </p>
           <p className="mb-4">
-            With a distinguished career spanning over three decades, Lord Smith has served in various governmental and
-            non-governmental roles, focusing primarily on economic development, environmental sustainability, and public
-            service reform.
+           At the age of 16, I began my first apprenticeship with The Quill C of E Trust, which lasted for one year and six months. During this time, I introduced revolutionary tools such as automation and scripting to the organisation, spearheaded the planning and delivery of HaloITSM within our IT Services Department for efficient technical support management, and led the technical infrastructure area to ensure that both pupils and staff had access to the resources necessary to effectively support teaching and learning.
           </p>
           <p className="mb-4">
-            Prior to joining the House of Lords, Lord Smith held several senior positions including Chief Executive of a
-            major public body and Chair of an international charitable foundation. His expertise in policy development
-            and implementation has been recognized through numerous awards and appointments.
+            In March, before completing my apprenticeship, I moved jobs to Albany Learning Trust, where, as ICT Technician, I will support the provision of servers, end-user computers, mobile devices, laptops, projectors, IWBs, and enterprise network equipment while the trust transitions to a centralised network, a task in which I possess significant prior experience before assuming this position.
           </p>
           <p className="mb-4">
-            Lord Smith holds a Master's degree in Public Administration from the London School of Economics and a
-            Bachelor's degree in Politics, Philosophy and Economics from the University of Oxford.
+            Throughout my still-evolving career, I have presented at several events, including the ANME (Association of Network Managers Event) on two occasions! Additionally, I presented at a Cybrewery Evening hosted by YorCyberSec, a truly innovative and engaging event for all IT and Cybersecurity professionals to share knowledge across the IT community—something I strongly believe in.
           </p>
           <p>
-            As a member of the House of Lords, Lord Smith contributes to debates, scrutinizes legislation, and serves on
-            select committees, bringing a wealth of practical experience to the legislative process.
+            I am deeply passionate about cloud and hybrid technologies and their transformative potential in driving organisational growth. By helping organisations adapt to rapid technological changes, I strive to unlock new opportunities for innovation, resilience, and future-proofing their technological capabilities.
+
+My career aspirations are to move to London, Westminster and work either in government, Houses of Parliament, or for the royal household.
           </p>
         </div>
       </div>
