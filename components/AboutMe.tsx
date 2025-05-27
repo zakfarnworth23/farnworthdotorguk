f421c6ee648e37cd7d567c076d5e164d0cab6cce
Zak Farnworth <130570087+zakfarnworth23@users.noreply.github.com>
2025-05-27 12:41:32 +0100

    Create AboutMe.tsx

---
+++ components/AboutMe.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Page } from 'govuk-react';
+
+const AboutMe = () => (
+  <Page>
+    <h1 className="govuk-heading-xl">About Me</h1>
+    <p className="govuk-body-l">
+      I'm Zak, a technology professional passionate about improving public services through modern, efficient, and secure digital solutions.
+    </p>
+    <div className="govuk-inset-text">
+      I believe in user-centered design, transparency, and using open standards to deliver services that just work.
+    </div>
+  </Page>
+);
+
+export default AboutMe;
