2f7e8a3d6c0fc92c8def1f75ce8b3c41c2920024
Zak Farnworth <130570087+zakfarnworth23@users.noreply.github.com>
2025-05-27 12:42:52 +0100

    Create ContactCard.tsx

---
+++ components/ContactCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ContactCard = () => (
+  <div className="govuk-width-container govuk-!-margin-top-6">
+    <h2 className="govuk-heading-l">Get in Touch</h2>
+    <p className="govuk-body">
+      Feel free to connect with me via email at{' '}
+      <a href="mailto:zak@farnworth.org.uk" className="govuk-link">
+        zak@farnworth.org.uk
+      </a>.
+    </p>
+  </div>
+);
+
+export default ContactCard;
